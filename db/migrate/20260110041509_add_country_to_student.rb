ba3d566d55d9ccedf2feab43571694dde6534650
Fidel Hernandez <fhernandez@gruposi.com>
2026-02-03 11:57:37 -0600

    fix migration add country to customer

--- db/migrate/20260110041509_add_country_to_student.rb
+++ db/migrate/20260110041509_add_country_to_student.rb
@@ -1,6 +1,5 @@
 class AddCountryToStudent < ActiveRecord::Migration[7.1]
   def change
     add_reference :students, :country, null: false, foreign_key: true
-    add_foreign_key :students, :countries
   end
 end
